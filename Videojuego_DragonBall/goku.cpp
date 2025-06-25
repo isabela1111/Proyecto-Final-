15ac1dd0e7b45a8db56b515deef70c8059e6fbed
juanFelipe2205 <153253730+juanFelipe2205@users.noreply.github.com>
2025-06-25 18:10:13 -0500

    Actualizando logica de la clase goku para que cumpla algunas funciones basicas de movimiento.

--- Videojuego_DragonBall/goku.cpp
+++ Videojuego_DragonBall/goku.cpp
@@ -1,25 +1,77 @@
 #include "goku.h"
-#include "recursos.h"
+#include <QKeyEvent>
 #include <QDebug>
 
 Goku::Goku(QGraphicsView* vista, QObject* parent)
-    : Personaje(vista, parent)
+    : Personaje(vista, parent),
+      frameActual(0),
+      tiempoSalto(0),
+      enElAire(false)
 {
-    spriteX = 0;
-    spriteY = 0;
+    nombre = "Goku";
+    vida = 5;
+    velocidad = 7;
     spriteAncho = 60;
     spriteAlto = 60;
-    vida = 3;
-    tieneNubeVoladora = false;
-    modoAtaque = false;
 
-    hojaSprites.load(Recursos::gokuSprite);
-    sprite = hojaSprites.copy(spriteX, spriteY, spriteAncho, spriteAlto);
-    setPixmap(sprite);
-    setPos(100, 300);  // Posición inicial
+    hojaSprites.load(":/Recursos/Sprites/goku.png");
+    setPixmap(hojaSprites.copy(0, 0, spriteAncho, spriteAlto));
+    setPos(100, 400);
     setFlag(QGraphicsItem::ItemIsFocusable);
     setFocus();
 }
+
+void Goku::mover() {
+    frameActual = (frameActual + 1) % 4;
+    setPixmap(hojaSprites.copy(frameActual * spriteAncho, 0, spriteAncho, spriteAlto));
+    moveBy(velocidad, 0);
+}
+
+void Goku::saltar() {
+    if (!enElAire) {
+        tiempoSalto = 15;
+        enElAire = true;
+    }
+}
+
+void Goku::atacar() {
+    qDebug() << "Goku realiza un ataque básico.";
+    setPixmap(hojaSprites.copy(0, 60, spriteAncho, spriteAlto));
+}
+
+void Goku::keyPressEvent(QKeyEvent* event) {
+    switch (event->key()) {
+    case Qt::Key_A: // izquierda
+        if (x() > 0) moveBy(-velocidad, 0);
+        mover();
+        break;
+    case Qt::Key_D: // derecha
+        if (x() + boundingRect().width() < limites.width()) moveBy(velocidad, 0);
+        mover();
+        break;
+    case Qt::Key_Space:
+        atacar();
+        break;
+    case Qt::Key_W:
+        saltar();
+        break;
+    default:
+        break;
+    }
+
+    // Simulación simple de salto
+    if (tiempoSalto > 0) {
+        moveBy(0, -10);
+        tiempoSalto--;
+    } else if (enElAire) {
+        moveBy(0, 10); // gravedad
+        if (y() >= 400) {
+            enElAire = false;
+            setY(400);
+        }
+    }
+}
+
 
 void Goku::usarNube() {
     tieneNubeVoladora = true;
