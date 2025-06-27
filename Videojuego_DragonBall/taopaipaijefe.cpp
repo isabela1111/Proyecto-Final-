d959f74f473b5faa8b962f2fb19266fd75b9d9ad
juanFelipe2205 <153253730+juanFelipe2205@users.noreply.github.com>
2025-06-27 12:30:44 -0500

    Implementar el movimiento de taopaipai en el nivel 2

--- Videojuego_DragonBall/taopaipaijefe.cpp
+++ Videojuego_DragonBall/taopaipaijefe.cpp
@@ -1,11 +1,12 @@
 #include "taopaipaijefe.h"
 #include "recursos.h"
 
-#include <QTimer>
+#include <QGraphicsScene>
 #include <QDebug>
 
 TaoPaiPaiJefe::TaoPaiPaiJefe(QGraphicsView* vista, QObject* parent)
-    : Personaje(vista, parent), puedeAtacar(true) {
+    : Personaje(vista, parent), puedeAtacar(true), objetivoJugador(nullptr) {
+
     nombre = "Tao Pai Pai";
     vida = 5;
     velocidad = 3;
@@ -16,8 +17,21 @@ TaoPaiPaiJefe::TaoPaiPaiJefe(QGraphicsView* vista, QObject* parent)
     spriteAncho = 60;
     spriteAlto = 60;
 
-    sprite = hojaSprites.copy(spriteX, spriteY, spriteAncho, spriteAlto);
-    setPixmap(sprite);
+    setPixmap(hojaSprites.copy(spriteX, spriteY, spriteAncho, spriteAlto));
+    setPos(600, 400);
+
+    setFlag(QGraphicsItem::ItemIsFocusable);
+    setFocus();
+
+    // Ataques periódicos
+    QTimer* ataqueTimer = new QTimer(this);
+    connect(ataqueTimer, &QTimer::timeout, this, &TaoPaiPaiJefe::atacar);
+    ataqueTimer->start(2500); // Cada 2.5 segundos
+
+    // Movimiento automático
+    movimientoTimer = new QTimer(this);
+    connect(movimientoTimer, &QTimer::timeout, this, &TaoPaiPaiJefe::mover);
+    movimientoTimer->start(1800);
 }
 
 void TaoPaiPaiJefe::atacar() {
@@ -32,6 +46,8 @@ void TaoPaiPaiJefe::lanzarGranada() {
     Granada* granada = new Granada(x(), y(), objetivoJugador);
     emit granadaLanzada(granada);
 
+    if (scene()) scene()->addItem(granada);
+
     puedeAtacar = false;
     QTimer::singleShot(1500, this, [this]() {
         puedeAtacar = true;
@@ -41,4 +57,19 @@ void TaoPaiPaiJefe::lanzarGranada() {
 void TaoPaiPaiJefe::setObjetivo(Personaje* objetivo) {
     objetivoJugador = objetivo;
 }
+
+void TaoPaiPaiJefe::mover() {
+    // Movimiento simple lateral hacia Goku (aleatorio o alternante si se prefiere)
+    if (!objetivoJugador) return;
+
+    if (x() > objetivoJugador->x()) {
+        moveBy(-velocidad, 0);
+    } else if (x() < objetivoJugador->x()) {
+        moveBy(velocidad, 0);
+    }
+    
+    spriteX = (spriteX + 1) % 4;
+    setPixmap(hojaSprites.copy(spriteX * spriteAncho, 0, spriteAncho, spriteAlto));
+}
+
 
