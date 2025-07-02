686946a2d8d956e2cf2f8c83a9454358674ca73b
juanFelipe2205 <153253730+juanFelipe2205@users.noreply.github.com>
2025-07-02 10:20:43 -0500

    Update avionenemigo.h

--- Videojuego_DragonBall/avionenemigo.h
+++ Videojuego_DragonBall/avionenemigo.h
@@ -6,29 +6,39 @@
 #include <QTimer>
 
 class GokuNube;
+class Misil;
 
-class AvionEnemigo : public QObject, public QGraphicsPixmapItem
-{
+class AvionEnemigo : public QObject, public QGraphicsPixmapItem {
     Q_OBJECT
 public:
     AvionEnemigo(GokuNube* goku, QGraphicsItem* parent = nullptr);
 
 signals:
     void colisionaConGoku();
+    void disparoMisil(Misil* misil);
 
 private slots:
     void mover();
     void animar();
+    void disparar();
 
 private:
     QTimer* timerMovimiento;
     QTimer* timerAnimacion;
+    QTimer* timerDisparo;
+
     QPixmap hojaSprites;
     int frameActual;
     int totalFrames;
     int spriteAncho;
     int spriteAlto;
     GokuNube* goku;
+
+    bool esEstacionario;
+    int misilesDisparados;
+    const int maxMisiles = 2;
+    bool haTerminadoDisparos = false;
 };
 
 #endif // AVIONENEMIGO_H
+
